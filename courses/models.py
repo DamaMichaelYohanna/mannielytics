9884b9207b182e6ca43914a320db1a17235646f4
godiesluvglobalconn025 <godiesluvglobalconn025@gmail.com>
2025-09-28 15:19:58 +0100

    udpated courses model

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     short_description = models.CharField(max_length=300, blank=True)
-    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='course_images/', blank=True, null=True,  storage=MediaCloudinaryStorage())  # Explicitly set the storage)
     features = models.TextField(help_text="Enter features separated by new lines", blank=True, null=True)
     duration = models.CharField(max_length=100, help_text="e.g., 3 months, 6 weeks", blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, null=True, blank=True)
